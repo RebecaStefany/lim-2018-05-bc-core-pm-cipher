797c2ca4889cfae3f2c65a35b8c8953393c42d01
rebecastefany <rs.tadeonicasio@hotmail.com>
2018-05-27 15:06:32 -0500

    si

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,39 @@
 window.cipher = {
-  // ... 
+  encode: (offset, string) => {
+    let cifrado = '';
+    offset = offset % 26;
+    for(let i=0; i < string.length; i++){
+      let ascii = string.charCodeAt(i);
+      console.log(ascii);
+      let formula;
+      if((ascii>=65) && (ascii<=90)){
+        formula = (ascii-65+offset)%26+65;
+      } else if((ascii>=97) && (ascii<=120)){
+        formula = (ascii-97+offset)%26+97;
+      }else{
+        formula=ascii;
+      }
+      cifrado += String.fromCharCode(formula);
+    }
+    return cifrado;
+  },
+
+  decode: (offset, string) =>{
+    let descifrado = '';
+    offset = offset % 26;
+    for(let i=0; i < string.length; i++){
+      let ascii = string.charCodeAt(i);
+      console.log(ascii);
+      let formula;
+      if((ascii>=65) && (ascii<=90)){
+        formula = (ascii-65-offset+26)%26+65;
+      } else if((ascii>=97) && (ascii<=122)){
+        formula = (ascii-97-offset+26)%26+97;
+      }else{
+        formula = ascii;
+      }
+      descifrado += String.fromCharCode(formula);
+    }
+    return descifrado;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 
-function cipher(){
+document.getElementById("encode").addEventListener("click", () =>{
+  const text = document.getElementById("text").value;
+  const valor = document.getElementById("valor").value;
+  const cifrado = document.getElementById("result");
+  cifrado.innerHTML = cipher.encode(valor, text);
+});
 
-    var textoCifrar = document.getElementById('textoCifrar').value;
-    var valorDesplazar = document.getElementById('numeroDesplazar').value;
-    var resultados = document.getElementById('Resultados');
-    var cifradas = '';
-
-    for (var i=0; i<textoCifrar.length; i++){
-      var formula = (textoCifrar.toUpperCase().charCodeAt(i) - 65 + valorDesplazar) % 26 + 65;
-      console.log(formula);
-      var cifrado = String.fromCharCode(formula);
-      cifradas += cifrado;
-      console.log(cifradas);
-     }
-     resultados.innerHTML = cifradas;
-   }
-
-   function decipher(){
-
-    var textoDescifrar = document.getElementById('textoCifrar').value;
-    var valorDesplazar = document.getElementById('numeroDesplazar').value;
-    var resultados = document.getElementById('Resultados');
-    var descifradas = '';
-
-    for (var j=0; j<textoDescifrar.length; j++){
-      var formulaDes = (textoDescifrar.toUpperCase().charCodeAt(j) + 65 - valorDesplazar) % 26 + 65;
-      var descifrado = String.fromCharCode(formulaDes);
-      descifradas += descifrado;
-     }
-     resultados.innerHTML = descifradas;
-   }
+document.getElementById("decode").addEventListener("click", () =>{
+  const text = document.getElementById("text").value;
+  const valor = document.getElementById("valor").value;
+  const descifrado = document.getElementById("result");
+  descifrado.innerHTML = cipher.decode(valor, text);
+});
