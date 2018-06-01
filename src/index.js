48298fe7b04ba665c9c367cb3aea08815642bfdc
rebecastefany <rs.tadeonicasio@hotmail.com>
2018-06-01 17:22:17 -0500

    si

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-
+//Devuelve una referencia al elemento por su ID
 document.getElementById("encode").addEventListener("click", () =>{
   const text = document.getElementById("text").value;
   const valor = document.getElementById("valor").value;
   const cifrado = document.getElementById("result");
   cifrado.innerHTML = cipher.encode(valor, text);
 });
-
+//Devuele una referencia al
 document.getElementById("decode").addEventListener("click", () =>{
   const text = document.getElementById("text").value;
   const valor = document.getElementById("valor").value;
